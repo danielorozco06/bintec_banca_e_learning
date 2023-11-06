d00e20bf485f2587752d1bbbd6ac4a58c3c4ff8a
Jose Eduardo Mogollon Jaimes <jemogoll@bancolombia.com.co>
2023-11-05 21:35:30 -0500

    unir perfil chatbot, arreglo de perfil variable

--- src/perfil.py
+++ src/perfil.py
@@ -7,7 +7,7 @@ p4 = "Cuantos ingresos tienes?"
 
 
 def save_data(r1, r2, r3, r4):
-    with open(f"perfiles/{r1}.txt", "a") as file:
+    with open(f"../perfiles/{r1}.txt", "w") as file:
         file.write(f"{p1}:{r1}\n{p2}:{r2}\n{p3}:{r3}\n{p4}:{r4}\n")
     return "Data saved successfully."
 
